bc3ff00985345ca67fbb3af7619e550555b02ae4
sorrowbeaver <jkj8790@gmail.com>
2017-02-03 15:30:58 +0900

    메시지 권한 기능 완료

--- app/src/main/java/morc/helpme/kr/morc/MainActivity.java
+++ app/src/main/java/morc/helpme/kr/morc/MainActivity.java
@@ -1,13 +1,19 @@
 package morc.helpme.kr.morc;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 import java.io.IOException;
 import morc.helpme.kr.morc.retrofit.HelpmeService;
 import okhttp3.ResponseBody;
@@ -18,18 +24,16 @@ import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
+  private Retrofit retrofit;
+  private HelpmeService helpmeService;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    //TODO 통신 확인 해볼 필요 있음
-    Retrofit retrofit = new Retrofit.Builder()
-        .baseUrl("http://192.168.1.11:9000")
-        .build();
-
-    final HelpmeService helpmeService = retrofit.create(HelpmeService.class);
+    initalizeLibraries();
 
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +50,41 @@ public class MainActivity extends AppCompatActivity {
         });
       }
     });
+  }
+
+  private void initalizeLibraries() {
+    initializeRetrofit();
+    initalizeDexter();
+  }
+
+  private void initializeRetrofit() {
+    //TODO 통신 확인 해볼 필요 있음
+    retrofit = new Retrofit.Builder()
+        .baseUrl("http://192.168.1.11:9000")
+        .build();
+
+    helpmeService = retrofit.create(HelpmeService.class);
+  }
+
+  private void initalizeDexter() {
+    Dexter.withActivity(this)
+        .withPermission(Manifest.permission.RECEIVE_SMS)
+        .withListener(new PermissionListener() {
+          @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+
+          }
+
+          @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+
+          }
+
+          @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission,
+              PermissionToken token) {
+
+          }
+        })
+        .check();
+
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
