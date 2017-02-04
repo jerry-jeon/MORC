a0c3af63bc48089233f43a39f4f9a9f705f136c0
sorrowbeaver <jkj8790@gmail.com>
2017-02-04 10:22:45 +0900

    receiver에서 통신

--- app/src/main/java/morc/helpme/kr/morc/receiver/SMSListener.java
+++ app/src/main/java/morc/helpme/kr/morc/receiver/SMSListener.java
@@ -6,18 +6,27 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.widget.Toast;
 import morc.helpme.kr.morc.Log;
+import morc.helpme.kr.morc.retrofit.HelpmeService;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class SMSListener extends BroadcastReceiver {
-  private SharedPreferences preferences;
+
+  private Retrofit retrofit;
+  private HelpmeService helpmeService;
 
   @Override
   public void onReceive(Context context, Intent intent) {
-    // TODO Auto-generated method stub
+    initializeRetrofit(context);
 
     if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
       Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
-      SmsMessage[] msgs = null;
+      SmsMessage[] msgs;
       String msg_from;
       if (bundle != null){
         //---retrieve the SMS message received---
@@ -28,12 +37,45 @@ public class SMSListener extends BroadcastReceiver {
             msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
             msg_from = msgs[i].getOriginatingAddress();
             String msgBody = msgs[i].getMessageBody();
-            Log.d(msgBody);
+            Toast.makeText(context, msgBody, Toast.LENGTH_LONG).show();
+            helpmeService.test().enqueue(new Callback<ResponseBody>() {
+              @Override
+              public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                Log.d("code : " + response.code());
+              }
+
+              @Override public void onFailure(Call<ResponseBody> call, Throwable t) {
+                t.printStackTrace();
+              }
+            });
           }
         }catch(Exception e){
           e.printStackTrace();
         }
       }
     }
+  }
+
+  private void initializeRetrofit(Context context) {
+
+    //TODO 통신 확인 해볼 필요 있음
+    if(retrofit == null) {
+      retrofit = new Retrofit.Builder().baseUrl("http://192.168.1.11:9000").build();
+    }
+
+    helpmeService = retrofit.create(HelpmeService.class);
+
+    /*
+    helpmeService.test().enqueue(new Callback<ResponseBody>() {
+      @Override
+      public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+        Log.d("code : " + response.code());
+      }
+
+      @Override public void onFailure(Call<ResponseBody> call, Throwable t) {
+        t.printStackTrace();
+      }
+    });
+    */
   }
 }
